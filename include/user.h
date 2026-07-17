8854dc10c6f50997df32d6b55a292b0cc4030345
Mohd Hamka <122353752+mhdhamka@users.noreply.github.com>
2026-07-17 20:46:38 +0800

    Refactor User class header file structure

--- include/user.h
+++ include/user.h
@@ -1,5 +1,4 @@
 
-/*Class User header file*/
 #ifndef USER_H
 #define USER_H
 
@@ -10,28 +9,22 @@ using namespace std;
 class User
 {
 	protected:
-		/*Protected attributes*/
+		
 		string username;
 		string password;
 		string email;
 
 	public:
-		//Public methods
-		
-		//Constructor
 		User();
 		
-		/*Setter*/
 		void setname(string name);
 		void setpassword(string p);
 		void setemail(string e);
 		
-		/*Getter*/
 		string getname();
 		string getpassword();
 		string getemail();
 		
-		//Public function
 		bool validateLogin(string name, string password);
 };
 
