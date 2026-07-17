94b45d506f9b536cfc57c8338711fead5790bbc0
Mohd Hamka <122353752+mhdhamka@users.noreply.github.com>
2026-07-17 20:44:12 +0800

    Refactor bool.h to declare user validation functions
    Removed implementation of user validation functions and added function declarations.

--- include/bool.h
+++ include/bool.h
@@ -1,78 +1,17 @@
-#include<stdbool.h>
-#include<string.h>
-#include<iostream>
-#include<cstring>
-#include<regex>
+#ifndef BOOL_H
+#define BOOL_H
 
-using namespace std;
+#include <string>
 
-bool checkExistUsername(const string& username)
-{
-	ifstream patReg("PatientRegistration.txt");
-	
-	int i = 0, j = 0;
-    string data[50][7];
-    while(getline(patReg, data[i][j]))
-    {
-        j++;
-        if(j>7){i++; j = 0;}
-    }
-        
-    patReg.close();
+bool checkExistUsername(const std::string& username);
 
-    for(int x=0; x<=i; x++)
-    {
-        if(data[x][0]==username)
-        {
-        	return false;
-        }
-        else
-        {
-            if(x==i)
-            {
-            	return true;
-            }
-        }
-    }
-}
+bool checkEmail(const std::string& email);
 
-bool checkEmail(const string& email)
-{
-	const regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
-	
-	return regex_match(email, pattern);
-}
+bool checkMobile(std::string input, int size);
 
-bool checkMobile(string input, int size)
-{
-	int i;
-	bool valid = true;
-	
-	for(i=0; i<=size; i++)
-	{
-		if(input[i] == '\0')
-		{
-			break;	
-		}
-		
-		valid = valid && isdigit(input[i]);
-	}
-	
-	const regex pattern("(01[^1])[0-9]{7}");
-	const regex pattern2("(011)[0-9]{8}");
-	
-	if(regex_match(input, pattern)||regex_match(input, pattern2))
-	{
-		return true;
-	}
-	
-	else
-	{
-		return false;
-	}
-	
-	return valid;
-}
+bool validateLogin(std::string username, std::string password);
 
-bool validateLogin(string username, string password);
+#endif
+
+
 
